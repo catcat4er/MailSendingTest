eb338c6d6580409a51783345bd73d1ab8c877f38
catcat4er <kerimov-t90@mail.ru>
2022-12-24 20:15:32 +0400

    added and split some new finish steps

--- src/test/java/MailSendingTest.java
+++ src/test/java/MailSendingTest.java
@@ -1,51 +1,71 @@
 import ConfigText.InsertingText;
+import com.codeborne.selenide.Configuration;
 import io.qameta.allure.Owner;
-
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.step;
-
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 @Owner("c@tcat4er")
 @DisplayName("Try to send the message")
 public class MailSendingTest {
 
+    String BaseURL = ("https://account.mail.ru/");
     InsertingText insText = ConfigFactory.create(InsertingText.class);
-
-
 
     @Test
     public void MailSendingTestWithSteps() {
 
+        Configuration.browserSize = "1920x1080";
+
         String login = insText.login();
         String password = insText.password();
         String message = insText.message();
+        String recipient = insText.recipient();
+        String topic = insText.topic();
+        String github = insText.github();
+        String meta = insText.meta();
 
-        step("open mail.ru", () -> {
-            open("https://account.mail.ru/");
-        });
+        step("open mail.ru", () -> open(BaseURL));
 
         step("authorisation", () -> {
-
             $("[name=username]").setValue(login);
             $("[data-test-id=next-button]").click();
             $("[name=password]").setValue(password);
             $("[data-test-id=submit-button]").click();
-            sleep(3000);
-            $(".compose-button__wrapper").click();
-            $(".head_container--3W05z .container--H9L5q").setValue("1111");
-            $(".subject__container--HWnat .container--H9L5q").setValue("1111");
-            $("[role='textbox']").setValue(message);
-            $(".vkuiButton__in").click();
-            $("#dimmer").shouldBe(visible);
-            $("[]");
-//
         });
 
+        sleep(3000); //знаю что слип это пошло, но сайт не реагирует на другие асерты.
+
+        step("check successful authorisation", () -> {
+            $(".compose-button__wrapper .compose-button__txt").shouldHave(text("Написать письмо"));
+        });
+
+        step("writing letter", () -> {
+            $(".compose-button__wrapper").click();
+            $(".head_container--3W05z .container--H9L5q").setValue(recipient);
+            $(".subject__container--HWnat .container--H9L5q").setValue(topic);
+            $("[role='textbox']").toWebElement().sendKeys(message + meta);
+        });
+
+        step("insert some links",() -> {
+            $("[title = 'Вставить ссылку']").click();
+            $("[data-test-id = link]").setValue(github);
+            $("[data-test-id = text]").setValue("Ссылка на работу в Гитхабе");
+            $("[data-test-id=save] .vkuiBut1ton__in").click();
+            $(".vkuiButton__in").click();
+        });
+
+        step("check successful mail sending",() -> {
+            $(".layer__link").shouldHave(text("Письмо отправлено"));
+        });
+
+
+
     }
+
+
 }
