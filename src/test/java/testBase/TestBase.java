8a7fdac07b767c357cee30cea54850acb0f607c7
catcat4er <kerimov-t90@mail.ru>
2022-12-27 22:28:48 +0400

    added generic attachments for allure report / increase TestBase methods

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -1,7 +1,13 @@
 package testBase;
 
+import attach.AttachHelpers;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -12,6 +18,23 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        AttachHelpers.screenshotAs("Last screenshot");
+        AttachHelpers.pageSource();
+        AttachHelpers.browserConsoleLogs();
+        AttachHelpers.addVideo();
     }
 
 }
